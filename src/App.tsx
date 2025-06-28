be327985d2d2240a3d42e6606350f5d860a220f8
afiatamanna06 <afiatamanna06@gmail.com>
2025-06-28 15:26:40 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      loading...
+    </>
+  )
+}
+
+export default App
