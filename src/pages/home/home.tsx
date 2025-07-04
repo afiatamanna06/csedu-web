b46f400deec0a9f2f096eb25a88d1df5ff9e9f56
afiatamanna06 <afiatamanna06@gmail.com>
2025-07-04 15:36:08 +0600

    Add academic programs

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,11 @@
+import AcademicPrograms from "@/components/home/academic-programs";
 import GoogleMap from "@/components/home/map";
 
 export default function Home() {
   return (
     <div className="w-full mt-[4rem] lg:mt-[8rem]">
+      <AcademicPrograms />
       <GoogleMap />
     </div>
-  )
+  );
 }
