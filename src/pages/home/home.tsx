39e3ece1d35e089346629836bff42f9d8e73bc30
afiatamanna06 <afiatamanna06@gmail.com>
2025-07-07 00:07:28 +0600

    Fix research section

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,6 +4,7 @@ import { DepartmentOverview } from "@/components/home/landing";
 import GoogleMap from "@/components/home/map";
 import News from "@/components/home/news";
 import Notices from "@/components/home/notices";
+import ResearchOpportunities from "@/components/home/research-opportunities";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <DepartmentOverview />
       <ExcellenceInEducation />
       <AcademicPrograms />
+      <ResearchOpportunities />
       <News />
       <Notices />
       <GoogleMap />
